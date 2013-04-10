d741684a15b9a7a17d322ef2f4290a38c4ad1bbe
Sean Laurent <organicveggie@gmail.com>
2013-04-10 14:49:47 -0700

    Rename Java packages.

--- src/main/java/com/bealetech/metrics/reporting/StatsdReporter.java
+++ src/main/java/com/bealetech/metrics/reporting/StatsdReporter.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2012 Sean Laurent
+ * Copyright (C) 2012-2013 Sean Laurent
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
-package com.studyblue.metrics.reporting;
+package com.bealetech.metrics.reporting;
 
 import com.yammer.metrics.Metrics;
 import com.yammer.metrics.core.*;
